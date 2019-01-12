21dedd09986a6326a874542fe86c5bddfe42d268
ivrunner <richyentam@gmail.com>
2019-01-12 10:52:41 -0500

    fixed topPic

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import topPic from "../images/top pic.jpg";
+import topPic from "../images/top-pic.jpg";
 
 const IndexPage = () => (
   <Layout>
